1e018b075699d00ec1a94c83e62cbec6f9c06d5d
Matthew Schroyer <mschroyer@gmail.com>
2025-01-28 16:55:37 -0500

    chore: update base in vite config for gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    port: 3000,
-  },
+    base: "/buddy-cue/",
+    build: {
+        outDir: 'build',
+    },
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
 });
