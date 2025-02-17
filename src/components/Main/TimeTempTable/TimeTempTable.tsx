7e22b65e2e1f3438bbd788e20a1817e49cdfa7ae
Matthew Schroyer <mschroyer@gmail.com>
2025-02-17 14:38:14 -0500

    refactor: make TimeTempTableHeader

--- src/components/Main/TimeTempTable/TimeTempTable.tsx
+++ src/components/Main/TimeTempTable/TimeTempTable.tsx
@@ -1,24 +1,13 @@
 import React from "react";
-import {
-  Table,
-  TableCell,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { Table } from "@mui/material";
 import TimeTempTableBody from "./TimeTempTableBody";
+import TimeTempTableHeader from "./TimeTempTableHeader";
 
 const TimeTempTable: React.FC = () => {
 
   return (
     <Table className="temp-table" aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Time</TableCell>
-          <TableCell>Temperature</TableCell>
-          <TableCell>Temp Difference</TableCell>
-          <TableCell>Coals Added</TableCell>
-        </TableRow>
-      </TableHead>
+      <TimeTempTableHeader />
       <TimeTempTableBody />
     </Table>
   );
